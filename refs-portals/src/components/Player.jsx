24df94cc226de08f5248de891740d9974993b9e9
Yakov <yakov.shev@gmail.com>
2023-11-02 18:33:59 +0100

    Enter username first version

--- refs-portals/src/components/Player.jsx
+++ refs-portals/src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [username, setUsername] = useState('');
+  const [submittedUsername, setSubmittedUsername] = useState(false);
+
+  const handleUsernameChange = (event) => {
+    setSubmittedUsername(false);
+    setUsername(event.target.value);
+  }
+
+  const handleUsernameSubmit = () => {
+    setSubmittedUsername(true);
+  }
+  
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome, {submittedUsername ? username : 'unknown entity'}!</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleUsernameChange} value={username} />
+        <button onClick={handleUsernameSubmit}>Set Name</button>
       </p>
     </section>
   );
