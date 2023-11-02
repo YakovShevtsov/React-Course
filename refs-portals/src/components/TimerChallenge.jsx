d389f55478910cd18bbe40f01b8c183d14aaa0ce
Yakov <yakov.shev@gmail.com>
2023-11-02 22:00:58 +0100

    Added ability to set a timer

--- refs-portals/src/components/TimerChallenge.jsx
+++ refs-portals/src/components/TimerChallenge.jsx
@@ -1,4 +1,17 @@
+import { useState } from "react";
+
 const TimerChallenge = ({title, targetTime}) => {
+    const [isTimerStarted, setIsTimerStarted] = useState(false);
+    const [isTimerExpired, setIsTimerExpired] = useState(false);
+
+
+    const handleStartTimer = () => {
+        setTimeout(() => {
+            setIsTimerExpired(true);
+        }, targetTime * 1000)
+        setIsTimerStarted(true);
+    }
+
     return(
         <section className="challenge">
             <h2>{title}</h2>
@@ -6,13 +19,14 @@ const TimerChallenge = ({title, targetTime}) => {
                 {targetTime} second{targetTime > 1 ? 's' : ''}
             </p>
             <p>
-                <button>
-                    Start challenge
+                <button onClick={handleStartTimer}>
+                    {isTimerStarted ? 'Stop' : 'Start'} challenge
                 </button>
             </p>
-            <p className="">
-                Time is running / Timer inactive
+            <p className={isTimerStarted ? 'active' : undefined}>
+                {isTimerStarted ? 'Time is running' : 'Timer inactive'}
             </p>
+            {isTimerExpired && <p>You lost</p>}
         </section>
     );
 }
